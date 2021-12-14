8370436bb8ff29aa5b21f6f9812dbd0fa3ea86bc
Стёпин Станислав Александрович <stanislavalex@mail.ru>
2021-12-14 21:52:21 +0300

    .

--- PrintFirstResultOfAsyncWebsitesDownloading/Program.cs
+++ PrintFirstResultOfAsyncWebsitesDownloading/Program.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using System.Net.Http;
+using System.Threading;
 
 namespace PrintFirstResultOfAsyncWebsitesDownloading
 {
@@ -14,18 +15,16 @@ namespace PrintFirstResultOfAsyncWebsitesDownloading
             var client = new HttpClient();
             Console.WriteLine("Enter list of URL for downloading and separate them by space:");
             string[] urls = (Console.ReadLine()).Split(' ');
-            Task[] tasks = new Task[urls.Length];
-            int taskNumber = 0;
+            List<Task> tasks = new List<Task> { };
             foreach (string url in urls)
             {
-                tasks[taskNumber] = Task.Run(async () =>
+                tasks.Add(Task.Run(async () =>
                 {
                     return await client.GetAsync(url);
-                });
-                taskNumber++;
+                }));
             }
-            var firstFinishedTask = Task.WaitAny(tasks);
-            Console.WriteLine($"Size of first downloaded website is: {tasks[firstFinishedTask]}");
+            var firstFinishedTask = await Task.WhenAny(tasks);
+            Console.WriteLine($"Size of first downloaded website is: {firstFinishedTask}");
 
             Console.ReadKey();
         }
